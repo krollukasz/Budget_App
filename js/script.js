cb42336302b25df8092b0dda1444ae8a8ccc202f
Łukasz Król <lionel@o2.pl>
2020-01-24 18:09:49 +0100

    Create an initialization function

--- js/script.js
+++ js/script.js
@@ -34,12 +34,20 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };  
 
   var ctrlAddItem = function() {
     // 1. Get the field input data
     var input = UICtrl.getInput();
-    console.log(input);
 
     // 2. Add the item to the budget controller
 
@@ -50,20 +58,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 5. Display the budget on the UI
   };
 
-  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
-
-  document.addEventListener("keypress", function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: function() {
+      setupEventListeners();
     }
-  })
+  }
 
+  })(budgetController, UIController);
 
-
-
-
-})(budgetController, UIController);
-
-
-
-
+controller.init();
