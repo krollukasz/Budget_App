2f9c6cbfb41510a19b9de6ede7552fdf98064adb
Łukasz Król <lionel@o2.pl>
2020-01-31 16:37:08 +0100

    Add delete items from budget controller and from the UI

--- js/script.js
+++ js/script.js
@@ -59,6 +59,21 @@ var budgetController = (function() {
       return newItem;
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      };
+
+    },
+
     calculateBudget: function() {
       
       // Calculate total incomes and expenses
@@ -141,6 +156,11 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
 
+    deleteListItem: function(selectorID) {
+      var element = document.getElementById(selectorID);
+      element.parentNode.removeChild(element);
+    },
+    
     clearFields: function()  {
       var fields, fieldsArr;
 
@@ -233,16 +253,16 @@ var controller = (function(budgetCtrl, UICtrl) {
 
       splitID = itemID.split("-");
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]);
 
       // 1. Delete the item from the data structure
-
+      budgetCtrl.deleteItem(type, ID);
 
       // 2. Delete the item from the UI
-
+      UICtrl.deleteListItem(itemID);
 
       // 3. Update and show the new budget
-
+      updateBudget();
     };
   };
 
